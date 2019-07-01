6ae055e3f4d0e62e3565bd44be7aa604aebdd5dd
LucasBrito <lucasbsilva29@gmail.com>
2019-07-01 09:51:09 -0300

    IImportando modulos de Http Client no teste

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [HttpClient, HttpHandler]
+  }));
 
   it('should be created', () => {
     const service: ApiService = TestBed.get(ApiService);
