7a93fd41ebf95bc5e250d239bb60d535cbd12c4f
LucasBrito <lucasbsilva29@gmail.com>
2019-07-01 23:46:40 -0300

    add API call

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { Storage } from '@ionic/storage';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class PhotoService {
 
   public photos: Photo[] = [];
 
-  constructor(private camera: Camera, private storage: Storage) { }
+  constructor(private camera: Camera, private storage: Storage, private apiService: ApiService) { }
 
   takePicture() {
     const options: CameraOptions = {
@@ -25,13 +26,23 @@ export class PhotoService {
         data: 'data:image/jpeg;base64,' + imageData
       });
 
+      this.processPhoto(imageData);
+
       // Save all photos for later viewing
       this.storage.set('photos', this.photos);
     }, (err) => {
-     // Handle error
-     console.log('Camera issue: ' + err);
+      // Handle error
+      console.log('Camera issue: ' + err);
     });
 
+  }
+
+  processPhoto(photo) {
+    this.apiService.uploadFoto(photo).subscribe(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    });
   }
 
   loadSaved() {
