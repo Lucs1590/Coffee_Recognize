582149aef9b97adddd276808c3cef2a2564f9527
LucasBrito <lucasbsilva29@gmail.com>
2019-07-20 14:46:00 -0300

    incrementing tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -4,12 +4,27 @@ import { ApiService } from './api.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('ApiService', () => {
+  let service: ApiService;
+
   beforeEach(() => TestBed.configureTestingModule({
     providers: [HttpClient, HttpHandler]
   }));
 
   it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
+    service = TestBed.get(ApiService);
     expect(service).toBeTruthy();
+  });
+
+  describe('Testing Defined Values', () => {
+
+    it('API_URL should be equal in front', () => {
+      const url = 'http://localhost:3000';
+      expect(url).toEqual(service.API_URL);
+    });
+
+    it('getHeader should return object type', () => {
+      expect(typeof service.getHeader()).toEqual('object');
+    });
+
   });
 });
