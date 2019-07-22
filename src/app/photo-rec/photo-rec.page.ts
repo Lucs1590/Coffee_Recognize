d8965c0f36da73d510bd2a478f1b66002cceed72
LucasBrito <lucasbsilva29@gmail.com>
2019-07-22 16:42:17 -0300

    Function of buttons and importing toast

--- src/app/photo-rec/photo-rec.page.ts
+++ src/app/photo-rec/photo-rec.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
 import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-photo-rec',
@@ -10,13 +12,40 @@ import { Router } from '@angular/router';
 export class PhotoRecognize implements OnInit {
   currentImage: any;
 
-  constructor(public photoService: PhotoService, public router: Router) { }
+  constructor(
+    public photoService: PhotoService,
+    public router: Router,
+    public apiService: ApiService,
+    public toastController: ToastController) { }
 
   ngOnInit() {
     this.photoService.loadSaved();
   }
 
-  QuantifyComponent() {
+  quantifyComponent() {
     this.router.navigate(['/photo-quant']);
+  }
+
+  clearPhotos() {
+    this.photoService.photos = [];
+  }
+
+  sendPhotos() {
+    this.apiService.uploadLoteOfPhotos(this.photoService.photos).subscribe(data => {
+      console.log(data);
+      this.clearPhotos();
+      this.presentToast('Processing performed successfully. We will send you an email. 🎉');
+    }, err => {
+      console.log(err);
+      this.presentToast('We had an error uploading, please try again! 🥺');
+    });
+  }
+
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 }
