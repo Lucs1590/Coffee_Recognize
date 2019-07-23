09f116fae07df9177ec9b0eb86be4bde6b93b0d6
LucasBrito <lucasbsilva29@gmail.com>
2019-07-23 16:38:40 -0300

    sending phtoto to process

--- src/app/preview/preview.page.ts
+++ src/app/preview/preview.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UtilsService } from '../services/utils.service';
 import { NavParams } from '@ionic/angular';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-preview',
@@ -10,13 +11,36 @@ import { NavParams } from '@ionic/angular';
 export class PreviewPage implements OnInit {
 
   @Input() currentImage;
+  processedImage;
+  processed: boolean;
 
-  constructor(public utils: UtilsService) { }
+  constructor(
+    public utils: UtilsService,
+    public apiService: ApiService
+  ) { }
 
   ngOnInit() {
+    this.processed = false;
   }
 
   close() {
     this.utils.dismissModal();
+  }
+
+  sendPhoto() {
+    this.apiService.sendOnePhoto(this.currentImage).subscribe(data => {
+      // loading
+      this.processedImage = data;
+      this.processed = false;
+      this.utils.presentToast('Processing performed successfully. 🎉');
+    }, err => {
+      console.log(err);
+      this.utils.presentToast('We had an error uploading, please try again! 🥺');
+    });
+  }
+
+  quantifyComponent() {
+    this.close();
+    this.utils.QuantifyComponent(this.currentImage);
   }
 }
