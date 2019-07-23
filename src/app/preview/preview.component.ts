69cd35fc2309dc21eadf365e8666f511e36d52ee
LucasBrito <lucasbsilva29@gmail.com>
2019-07-23 14:18:41 -0300

    Creating photo preview functions

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilsService } from '../services/utils.service';
 
 @Component({
   selector: 'app-preview',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public utils: UtilsService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
