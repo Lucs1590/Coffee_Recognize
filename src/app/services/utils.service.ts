9e08f2431bcd59bf81a0ddbca747faa162214691
LucasBrito <lucasbsilva29@gmail.com>
2019-07-23 16:48:45 -0300

    changing message and remove log

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -42,14 +42,12 @@ export class UtilsService {
 
   async presentLoading() {
     const loading = await this.loadingController.create({
-      message: 'Hellooo',
+      message: 'Loading...',
       duration: 2000
     });
     await loading.present();
 
     const { role, data } = await loading.onDidDismiss();
-
-    console.log('Loading dismissed!');
   }
 
   RecognitionComponent() {
