3f82ae1448c7b8c560e3ddcb8c55b8169a432f56
LucasBrito <lucasbsilva29@gmail.com>
2019-07-23 16:37:15 -0300

    sending photo to quantify

--- src/app/photo-quant/photo-quant.page.ts
+++ src/app/photo-quant/photo-quant.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { UtilsService } from '../services/utils.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-photo-quant',
@@ -7,16 +8,32 @@ import { UtilsService } from '../services/utils.service';
   styleUrls: ['photo-quant.page.scss']
 })
 export class PhotoQuantify implements OnInit {
+  processedImage;
+  processed: boolean;
 
   constructor(
-    public utils: UtilsService
+    public utils: UtilsService,
+    public apiService: ApiService
   ) { }
 
   ngOnInit() {
-
+    // input
+    this.send_calcPhoto();
   }
 
   close() {
     this.utils.RecognitionComponent();
+  }
+
+  send_calcPhoto() {
+    this.apiService.send_calcOnePhoto(this.utils.currentImage).subscribe(data => {
+      // loading
+      this.processedImage = data;
+      this.processed = false;
+      this.utils.presentToast('Processing performed successfully. 🎉');
+    }, err => {
+      console.log(err);
+      this.utils.presentToast('We had an error uploading, please try again! 🥺');
+    });
   }
 }
