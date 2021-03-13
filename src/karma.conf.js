6307280d87ac5776cdff572a5767ae508f79fe0e
Lucas Brito <lucasbsilva29@gmail.com>
2021-03-13 09:40:52 -0300

    build: atualizando testes e ci

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,20 +1,62 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
+// karma.conf.js
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    files: [
+      '**/*.spec.ts'
+    ],
+
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [require('karma-jasmine'), require('karma-chrome-launcher'), require('karma-jasmine-html-reporter'), require('karma-coverage-istanbul-reporter'), require('karma-teamcity-reporter'), require('@angular-devkit/build-angular/plugins/karma')],
-    client: { clearContext: false }, // leave Jasmine Spec Runner output visible in browser
-    coverageIstanbulReporter: { dir: require('path').join(__dirname, '../coverage'), reports: ['html', 'lcovonly'], fixWebpackSourcePaths: true },
-    reporters: ['progress', 'kjhtml', 'teamcity'],
-    customLaunchers: { ChromeHeadlessNoSandbox: { base: 'ChromeHeadless', flags: ['--no-sandbox'] } },
+
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-coverage'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-teamcity-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+
     port: 9876,
+
     colors: true,
+
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+
+    reporters: [
+      'progress',
+      'kjhtml',
+      'teamcity'
+    ],
+
+    preprocessors: {
+      '**/*.spec.ts': ['coverage']
+    },
+
+    browsers: [
+      'Chrome',
+      'ChromeHeadless',
+      'ChromeHeadlessCI'
+    ],
+
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+
+    coverageReporter: {
+      dir: 'build/reports/coverage',
+      reporters: [
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
+    }
+
   });
 };
